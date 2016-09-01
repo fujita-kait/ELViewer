cd1114e70946db15dd0966c0ccb8e373c84ec4c7
藤田裕之 <me@hirofujita.com>
2016-09-01 15:09:12 +0900

    Initial Commit

---
+++ ELDeviceObjectsViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ELDeviceObjectsViewer
+//
+//  Created by 藤田裕之 on 2016/09/01.
+//  Copyright © 2016年 SmartHouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
