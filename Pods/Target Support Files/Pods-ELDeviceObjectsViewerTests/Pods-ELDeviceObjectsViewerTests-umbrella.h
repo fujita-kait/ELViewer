8c41ef84552431cc0c7a4bfd9f1921c144e84777
fujita-kait <hiro.fujita@he.kanagawa-it.ac.jp>
2016-09-07 17:49:03 +0900

    Commit pods

---
+++ Pods/Target Support Files/Pods-ELDeviceObjectsViewerTests/Pods-ELDeviceObjectsViewerTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ELDeviceObjectsViewerTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ELDeviceObjectsViewerTestsVersionString[];
+
